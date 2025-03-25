3468e8dbf534b80864c67e72cfada8d3316266f6
Ercole19 <71319051+Ercole19@users.noreply.github.com>
2025-03-25 17:21:56 +0100

    going back

--- src/main/java/com/maven_test/App.java
+++ src/main/java/com/maven_test/App.java
@@ -14,20 +14,5 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("This is a test for maven build");
-
-        try {
-            File file = new File("C:\\Users\\simon\\Desktop\\mavenTxt.txt"); // Adjust the path if necessary
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                System.out.println(line);
-            }
-
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-            e.printStackTrace();
-        }
     }
 }
