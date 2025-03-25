7aaaa17e536a8f55c9e53743179cdaa03c0ea846
Ercole19 <71319051+Ercole19@users.noreply.github.com>
2025-03-25 17:18:57 +0100

    build fail test

--- src/main/java/com/maven_test/App.java
+++ src/main/java/com/maven_test/App.java
@@ -1,5 +1,9 @@
 package com.maven_test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -10,5 +14,20 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("This is a test for maven build");
+
+        try {
+            File file = new File("data.txt"); // Adjust the path if necessary
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                System.out.println(line);
+            }
+
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+            e.printStackTrace();
+        }
     }
 }
